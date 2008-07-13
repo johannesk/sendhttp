48ea18884e8fa104d2836149b43863191b91c28c
Johannes Krude <johannes@krude.de>
2008-07-13 21:00:37 +0200

    added 10 stupid lines of code

--- httpserver.rb
+++ httpserver.rb
@@ -5,17 +5,24 @@ class HTTPServer
 		@files= []
 	end
 
-	def add_file(file)
-		@files<< file
-	end
-
-	def listen(port)
+	def listen(port, &block)
 		@listen= TCPServer.new(nil, port)
 		begin
-			while connection= @listen.accept_nonblock
+			while connection= @listen.accept
+				Thread.new(connection) do |c|
+					handle_connection(c, block)
+				end
 			end
 		rescue Errno::EAGAIN
 			retry
+		end
+	end
+
+	def handle_connection(connection, &block)
+		line= connection.readline.strip
+		if line=~ /^GET \/(.*?) HTTP\/1.[01]$/ and stream= block.call($1)
+		else
+			connection.write(header("400 Bad Request"))
 		end
 	end
 
