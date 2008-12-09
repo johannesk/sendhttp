32a4d2221de1c368813752f499cd868db7ea4589
Johannes Krude <johannes@krude.de>
2008-12-09 22:45:46 +0100

    fixed all license texts they now contain the right program name

--- html.rb
+++ html.rb
@@ -1,20 +1,20 @@
 #
 # © Johannes Krude 2008
 #
-# This file is part of sendfile-utils.
+# This file is part of sendhttp.
 #
-# broadcast-files is free software: you can redistribute it and/or modify
+# sendhttp is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# broadcast-files is distributed in the hope that it will be useful,
+# sendhttp is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with sendhttp.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 module HTML
